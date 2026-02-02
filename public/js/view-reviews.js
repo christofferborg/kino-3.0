946b41a7b345566ce05246a8ec9c95562d0cf1fa
FeliciaBo <theskylla@gmail.com>
2026-02-02 21:41:14 +0100

    added back button on mobile (hidden on desktop)

--- public/js/view-reviews.js
+++ public/js/view-reviews.js
@@ -1,6 +1,19 @@
- const btn = document.querySelector(".reviewBtn._view");
+ 
+ //button to access the view reviews page
+ const viewBtn = document.querySelector(".reviewBtn._view");
 
-  btn.addEventListener("click", () => {
-    const id = btn.dataset.id;
+  viewBtn.addEventListener("click", () => {
+    const id = viewBtn.dataset.id;
     window.location.href = `/richards-filmer/${id}/view-reviews`;
      });
+
+
+ //button to go back to previous page (movie info)
+  const backBbtn = document.querySelector(".backBtn");
+
+ if (backBtn) {
+  backBtn.addEventListener("click", () => {
+    history.back(); 
+  });
+}
+    
