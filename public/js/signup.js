cb19bbdddab49d5617f67d7f1ad9b9cbab07fd22
Kevinekmen <kevin.ekmen@lerniastudent.se>
2026-03-07 18:55:53 +0100

    kopplat fsingup. filerna till loginPage.js och profile.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -30,3 +30,59 @@ togglePassword.addEventListener("click", () => {
   // 3. Byt bildkälla
   eyeIcon.src = isPassword ? "./img/hide.png" : "./img/view.png";
 });
+
+ const signupForm = document.querySelector("form");
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const emailWarning = document.querySelector(".emailWarning");
+  const usernameWarning = document.querySelector(".userNameWarning");
+  const emailInput = document.querySelector("#email");
+
+  emailWarning.classList.add("hidden");
+  usernameWarning.classList.add("hidden");
+  emailInput.classList.remove("border-[#FA453E]");
+
+  const fname = document.getElementById("fname").value.trim();
+  const lname = document.getElementById("lname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const newUser = {
+    fname,
+    lname,
+    email,
+    username,
+    password,
+  };
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const usernameTaken = users.some(
+    (user) => user.username.toLowerCase() === username.toLowerCase()
+  );
+
+  const emailTaken = users.some(
+    (user) => user.email.toLowerCase() === email.toLowerCase()
+  );
+
+  if (usernameTaken) {
+    usernameWarning.textContent = "Användarnamnet är upptaget.";
+    usernameWarning.classList.remove("hidden");
+    return;
+  }
+
+  if (emailTaken) {
+    emailWarning.textContent = "E-postadressen används redan.";
+    emailWarning.classList.remove("hidden");
+    emailInput.classList.add("border-[#FA453E]");
+    return;
+  }
+
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+
+    window.location.href = "/login"
+  });
