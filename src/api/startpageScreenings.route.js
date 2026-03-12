57fe854aec9d3aa5de301ed2f6409fc3f980d681
lenzxn <lenson.gr@hotmail.com>
2026-03-12 04:09:58 +0100

    Final commit

--- src/api/startpageScreenings.route.js
+++ src/api/startpageScreenings.route.js
@@ -20,7 +20,6 @@ router.get("/screenings", async (req, res) => {
     );
 
     res.json(result);
-    console.log(result);
   } catch (error) {
     res.status(500).json({ error: "Could not load screenings" });
   }
